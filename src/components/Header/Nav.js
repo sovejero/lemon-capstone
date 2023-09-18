a7c6af52fe4f5b824a0b53a10a681e32a3490092
sovejero <santiagodovejero@gmail.com>
2023-09-17 22:55:45 -0300

    refactor: change NavLink over Link for nav

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Nav.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className="nav">
-            <Link className="nav-button nav-button--text" to="/home">Home</Link>
-            <Link className="nav-button nav-button--text" to="/about">About</Link>
-            <Link className="nav-button nav-button--text" to="/menu">Menu</Link>
-            <Link className="nav-button nav-button--text" to="/reservations">Reservations</Link>
-            <Link className="nav-button nav-button--text" to="/orderonline">Order Online</Link>
-            <Link className="nav-button nav-button--text" to="/login">Login</Link>
+            <NavLink className="nav-button nav-button--text" to="/">Home</NavLink>
+            <NavLink className="nav-button nav-button--text" to="#about">About</NavLink>
+            <NavLink className="nav-button nav-button--text" to="#menu">Menu</NavLink>
+            <NavLink className="nav-button nav-button--text" to="/booking">Reservations</NavLink>
+            <NavLink className="nav-button nav-button--text" to="##">Order Online</NavLink>
+            <NavLink className="nav-button nav-button--text" to="##">Login</NavLink>
         </nav>
     );
 }
