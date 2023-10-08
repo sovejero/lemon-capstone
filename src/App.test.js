4f07689ae66881df70e52864375341114e1cead3
sovejero <santiagodovejero@gmail.com>
2023-10-08 03:19:51 -0300

    fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Little Lemon headlines', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const reserveButton = screen.getAllByText(/Little Lemon/i, { selector: 'h1' });
+  expect(reserveButton[0]).toBeInTheDocument();
 });
--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,35 +1,45 @@
-/* eslint-disable testing-library/prefer-screen-queries */
-import { render, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import BookingForm from '../components/BookingForm';
+import { BrowserRouter } from "react-router-dom";
 
 describe('BookingForm', () => {
+
+  const MockBookingForm = () => {
+    const availableTimes = ['10:00', '11:00', '12:00'];
+    return(
+    <BrowserRouter> 
+      <BookingForm availableTimes={availableTimes} dispatch={()=>{}} />
+    </BrowserRouter>
+    )
+  }
+
   it('renders correctly', () => {
-    const { getByTestId } = render(<BookingForm />);
-    expect(getByTestId('booking-form')).toBeInTheDocument();
+    render(<MockBookingForm/>);
+    expect(screen.getByTestId('booking-form')).toBeInTheDocument();
   });
 
   it('allows the user to fill the form', () => {
-    const { getByLabelText } = render(<BookingForm />);
-    fireEvent.change(getByLabelText(/Choose date/i), { target: { value: '2023-09-20' } });
-    fireEvent.change(getByLabelText(/Choose time/i), { target: { value: '10:00' } });
-    fireEvent.change(getByLabelText(/Number of guests/i), { target: { value: '2' } });
-    fireEvent.change(getByLabelText(/Occasion/i), { target: { value: 'Birthday' } });
-    expect(getByLabelText(/Choose date/i).value).toBe('2023-09-20');
-    expect(getByLabelText(/Choose time/i).value).toBe('10:00');
-    expect(getByLabelText(/Number of guests/i).value).toBe('2');
-    expect(getByLabelText(/Occasion/i).value).toBe('Birthday');
+    render(<MockBookingForm/>);
+    fireEvent.change(screen.getByLabelText(/Choose date/i), { target: { value: '2023-09-20' } });
+    fireEvent.change(screen.getByLabelText(/Choose time/i), { target: { value: '10:00' } });
+    fireEvent.change(screen.getByLabelText(/Number of guests/i), { target: { value: '2' } });
+    fireEvent.change(screen.getByLabelText(/Occasion/i), { target: { value: 'Birthday' } });
+    expect(screen.getByLabelText(/Choose date/i).value).toBe('2023-09-20');
+    expect(screen.getByLabelText(/Choose time/i).value).toBe('10:00');
+    expect(screen.getByLabelText(/Number of guests/i).value).toBe('2');
+    expect(screen.getByLabelText(/Occasion/i).value).toBe('Birthday');
   });
 
   it('disables the submit button if the form is invalid', () => {
-    const { getByTestId } = render(<BookingForm />);
-    expect(getByTestId('submit-button')).toBeDisabled();
+    render(<MockBookingForm/>);
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
   });
 
   it('enables the submit button if the form is valid', () => {
-    const { getByTestId, getByLabelText } = render(<BookingForm />);
-    fireEvent.change(getByLabelText(/Choose date/i), { target: { value: '2023-09-20' } });
-    fireEvent.change(getByLabelText(/Choose time/i), { target: { value: '10:00' } });
-    fireEvent.change(getByLabelText(/Number of guests/i), { target: { value: '2' } });
-    expect(getByTestId('submit-button')).not.toBeDisabled();
+    render(<MockBookingForm/>);
+    fireEvent.change(screen.getByLabelText(/Choose date/i), { target: { value: '2023-09-20' } });
+    fireEvent.change(screen.getByLabelText(/Choose time/i), { target: { value: '10:00' } });
+    fireEvent.change(screen.getByLabelText(/Number of guests/i), { target: { value: '2' } });
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
   });
 });
